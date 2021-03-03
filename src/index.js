637edf89de35996489cdd15a8863b930381589ff
joshua mshana <mama27j@gmail.com>
2021-03-03 09:35:45 +0300

    add update notifier

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const os = require('os');
 const {join} = require('path');
 const file = require('fs-extra');
 const BfastTools = require('./bfast.cli');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
 const bfast = new BfastTools();
 (function () {
     const path = join(`${os.homedir()}`,'bfast-tools');
@@ -10,5 +12,9 @@ const bfast = new BfastTools();
         return;
     }
     file.mkdirsSync(path);
+})();
+(function () {
+    updateNotifier({pkg}).notify();
+
 })();
 bfast.start();
